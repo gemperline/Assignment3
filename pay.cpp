e35608653f4dd8d69627519be1c0a8e3825e1618
agemperline <agemperline@cs3002c.acad.fullerton.edu>
2019-03-12 17:24:17 -0700

    commit

--- pay.cpp
+++ pay.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 #include "person.cpp"
 
-int readData(vector<Person> P, string fileName, int &numPeople)
+void readData(vector<Person> &P, string fileName, int &numPeople)
 {
   string fName;
   string lName;
   float  hours;
   float  pay;
+  // create a person object, set read data to class memebers
+  Person p;
 
   ifstream inFile;
   inFile.open("input.txt");
@@ -27,13 +29,11 @@ int readData(vector<Person> P, string fileName, int &numPeople)
     {
       inFile >> lName >> hours >> pay;
 
-      // create a person object, set read data to class memebers
-      Person p;
       p.setFirstName(fName);
       p.setLastName(lName);
       p.setHoursWorked(hours);
       p.setPayRate(pay);
-      P.push_back(p);
+      p.push_back(P);
 
       // count the number of people in input file
       numPeople++;
@@ -42,12 +42,10 @@ int readData(vector<Person> P, string fileName, int &numPeople)
   }
   else
     cout << "ERROR: could not open file ..." << endl;
-
-    return numPeople;
 }
 
 
-void writeData(vector<Person> P, string fileName, int numPeople)
+void writeData(vector<Person> &P, string fileName, int numPeople)
 {
   ofstream outFile;
   outFile.open(fileName);
